2759369b6931c7afe3dd557d4cd78dd8477b7396
KouKouChan <1010433454@qq.com>
2020-10-22 15:28:18 +0800

    解析房间错误代码

--- blademaster/core/room/room.go
+++ blademaster/core/room/room.go
@@ -12,31 +12,22 @@ func OnRoomRequest(p *PacketData, client net.Conn) {
 	if p.PraseRoomPacket(&pkt) {
 		switch pkt.InRoomType {
 		case NewRoomRequest:
-			//log.Println("Recived a new room request from", client.RemoteAddr().String())
 			OnNewRoom(p, client)
 		case JoinRoomRequest:
-			//log.Println("Recived a join room request from", client.RemoteAddr().String())
 			OnJoinRoom(p, client)
 		case LeaveRoomRequest:
-			//log.Println("Recived a leave room request from", client.RemoteAddr().String())
 			OnLeaveRoom(client, false)
 		case ToggleReadyRequest:
-			//log.Println("Recived a ready request from", client.RemoteAddr().String())
 			OnToggleReady(p, client)
 		case GameStartRequest:
-			//log.Println("Recived a start game request from", client.RemoteAddr().String())
 			OnGameStart(p, client)
 		case UpdateSettings:
-			//log.Println("Recived a update room setting request from", client.RemoteAddr().String())
 			OnUpdateRoom(p, client)
 		case OnCloseResultWindow:
-			//log.Println("Recived a close resultWindow request from", client.RemoteAddr().String())
 			OnCloseResultRequest(p, client)
 		case SetUserTeamRequest:
-			//log.Println("Recived a set user team request from", client.RemoteAddr().String())
 			OnChangeTeam(p, client)
 		case GameStartCountdownRequest:
-			//log.Println("Recived a begin start game request from", client.RemoteAddr().String())
 			OnGameStartCountdown(p, client)
 		case Feedback:
 			OnRoomFeedback(p, client)
