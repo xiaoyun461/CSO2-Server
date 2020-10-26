e200a33acc0275d0e49db044d812bd2686979d7b
KouKouChan <1010433454@qq.com>
2020-10-26 22:12:37 +0800

    fix mutex bug

--- blademaster/typestruct/room.go
+++ blademaster/typestruct/room.go
@@ -349,6 +349,7 @@ func (rm Room) FindDesirableTeam() int {
 		} else if v.GetUserTeam() == UserForceCounterTerrorist {
 			ctNum++
 		} else {
+			rm.RoomMutex.Unlock()
 			DebugInfo(2, "Error : User", string(v.UserName), "is in Unknown team in room", string(rm.Setting.RoomName))
 			return 0
 		}
